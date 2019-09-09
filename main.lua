4dcbc779e0285470ff44bb5fde3adbc8481114dd
Mikel Duke <mikelduke@gmail.com>
2019-09-08 20:25:54 -0500

    draw to canvas instead

--- main.lua
+++ main.lua
@@ -1,51 +1,36 @@
-updateFreq = 0
+updateFreq = .1
 updateDiff = 0
-circles = {}
 max = 500
 maxDiam = love.graphics.getHeight() / 2
+
+c = love.graphics.newCanvas(love.graphics.getWidth(), love.graphics.getHeight())
 
 function love.load() end
 
 function love.update(dt)
     if love.keyboard.isDown('escape') then love.event.push('quit') end
 
-    if (#circles > max) then
-        for k, v in pairs(circles) do circles[k] = nil end
-    end
-
     updateDiff = updateDiff + dt
     if (updateDiff > updateFreq) then
         updateDiff = 0
-        addCircle()
+        c:renderTo(addCircle)
     end
 end
 
 function love.draw()
     love.graphics.setColor(1, 1, 1)
-    for i, s in ipairs(circles) do love.graphics.draw(s.image, s.x, s.y) end
+
+    love.graphics.draw(c)
 end
 
 function addCircle()
-    local circle = {
-        image = getShape(love.math.random(1, maxDiam), {
-            r = love.math.random(),
-            g = love.math.random(),
-            b = love.math.random()
-        }),
-        x = love.math.random(0, love.graphics.getWidth()),
-        y = love.math.random(0, love.graphics.getHeight())
-    }
-    table.insert(circles, circle)
-end
+    local r = love.math.random()
+    local g = love.math.random()
+    local b = love.math.random()
+    local x = love.math.random(0, love.graphics.getWidth())
+    local y = love.math.random(0, love.graphics.getHeight())
+    size = love.math.random(1, maxDiam)
 
-function getShape(size, color)
-    local shape = love.graphics.newCanvas(size, size)
-    love.graphics.setCanvas(shape)
-    love.graphics.setColor(color.r, color.g, color.b, 255)
-
-    local mode = "fill"
-    love.graphics.circle(mode, size / 2, size / 2, size / 2)
-
-    love.graphics.setCanvas()
-    return shape
+    love.graphics.setColor(r, g, b, 1)
+    love.graphics.circle("fill", x, y, size / 2)
 end
